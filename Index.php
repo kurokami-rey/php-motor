77543190749fd434c520115409aeeeef93a5026f
Kurokami_rey <56182144+kurokami-rey@users.noreply.github.com>
2019-10-21 09:17:44 +0700

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,19 @@
+<?php
+echo 'ini yang benar' ;
+echo "<br>" ;
+$a = 'beat' ;
+$b = 'vario' ;
+if ($a == 'beat'){
+	echo 'motor saya' ;
+}else{
+	echo 'bukan motor saya' ;
+}	
+echo "<br>" ;
+echo 'ini yang salah' ;
+echo "<br>" ;
+if ($b == 'beat'){
+	echo 'motor saya' ;
+}else{
+	echo 'bukan motor saya' ;
+}	
+?>
